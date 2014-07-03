434c553ce02f9cc01e2cd51761a989c78dc39045
Seth Miller <cr125rider@gmail.com>
2014-07-02 23:08:30 -0500

    Fixed "all" test to have at least one test.

--- test/all.js
+++ test/all.js
@@ -1,8 +1,19 @@
 var Cli = require('./../lib/cli'),
+	Command = require('./../lib/command'),
 	CliError = require('./../lib/error/abstract-error');
 
 exports.multiple1 = function(test) {
 	var cli = new Cli();
 	cli
-		.commandGroup()
+		.commandGroup('cmd', 'The primary command group', [
+			new Command('push', 'Push up to the remote'),
+			new Command('pull', 'Pull from the upstream')
+		], null, true)
+		.flag('force', 'Force the command to execute', '-f', '--force')
+		.option('remote', 'The remote repo to push to', '-r', '--remote', 'string')
+		.parse(['test.js', 'push', '-f', '-r', 'origin']);
+	test.equal(cli.params.cmd, 'push');
+	test.equal(cli.params.force, true);
+	test.equal(cli.params.remote, 'origin');
+	test.done();
 }
